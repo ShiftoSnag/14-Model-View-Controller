51e87ca1f2f0de5728a21450e945a4a0b9ba8033
Ryan Mattson <ryan.mattson32@gmail.com>
2023-10-01 00:55:58 -0600

    Create express

--- server.js
+++ server.js
@@ -6,3 +6,16 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({ helpers: require("./utils/helpers") });
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+// Setting up session object with secret, cookie, and store
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
