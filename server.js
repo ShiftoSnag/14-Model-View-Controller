953b0a167e49bda3100f5cf33c2f71b9f4917ff2
Ryan Mattson <ryan.mattson32@gmail.com>
2023-10-01 00:54:41 -0600

    Added modules

---
+++ server.js
@@ -0,0 +1,8 @@
+// Importing required modules and dependencies
+const express = require("express");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers: require("./utils/helpers") });
